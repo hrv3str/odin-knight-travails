0aba1ee2ce48f4cde945fdb50a8c19359d7bdeb0
hrv3str <r.novikov.zp.ua@gmail.com>
2023-10-03 18:07:44 +0300

    Test move module

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
 import 'normalize.css';
 import './styles.css';
+import Moves from './modules/move.js'
+
+const start = [0, 1];
+console.log(`Start position is ${start}`);
+const target = [4, 6];
+console.log(`Target position is ${target}`);
+const routes = new Moves(start);
+const result = routes.getRoute();
+console.log(result);

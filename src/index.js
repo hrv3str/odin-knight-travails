d8d826f680fbe35223d8812f357bc583450fbcc6
hrv3str <r.novikov.zp.ua@gmail.com>
2023-10-06 14:13:18 +0300

    Test UI module

--- src/index.js
+++ src/index.js
@@ -1,17 +1,8 @@
 import 'normalize.css';
 import './styles.css';
 import Chessboard from './modules/board.js'
+import Display from './modules/UI.js'
 
-const start = [3, 3];
-console.log(`Start position is [${start}]`);
-const target = [4, 6];
-console.log(`Target position is [${target}]`);
+const display = new Display;
 
-const board = new Chessboard(8);
-
-const route = board.pathFind(start, target);
-
-console.group('result');
-console.log('Shortest path is:');
-console.log(route);
-console.groupEnd()
+display.fillBoard();

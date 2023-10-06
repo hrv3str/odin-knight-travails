c5116344e556f08e1b47658c7e04ac37f5046d71
hrv3str <r.novikov.zp.ua@gmail.com>
2023-10-06 10:56:30 +0300

    Test board module

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import 'normalize.css';
 import './styles.css';
-import Moves from './modules/move.js'
+import Chessboard from './modules/board.js'
 
-const start = [0, 1];
-console.log(`Start position is ${start}`);
+const start = [3, 3];
+console.log(`Start position is [${start}]`);
 const target = [4, 6];
-console.log(`Target position is ${target}`);
-const routes = new Moves(start);
-const result = routes.getRoute();
-console.log(result);
+console.log(`Target position is [${target}]`);
+
+const board = new Chessboard(8);
+
+const route = board.pathFind(start, target);
+
+console.group('result');
+console.log('Shortest path is:');
+console.log(route);
+console.groupEnd()

a9f39a1fd50710e230b9ba2048bb946a453c89f4
hrv3str <r.novikov.zp.ua@gmail.com>
2023-10-11 15:00:26 +0300

    Test knight travail animations

--- src/index.js
+++ src/index.js
@@ -4,5 +4,16 @@ import Chessboard from './modules/board.js'
 import Display from './modules/UI.js'
 
 const display = new Display;
+const board = new Chessboard(8);
 
-display.handleElementsDragging();
+const testTravails = () => {
+    const start = display.start;
+    const finish = display.finish;
+    const result = board.pathFind(start, finish);
+    console.log(result);
+    display.handleTravail(result);
+}
+
+const button = document.getElementById("launch-button");
+
+button.addEventListener('click', testTravails);
